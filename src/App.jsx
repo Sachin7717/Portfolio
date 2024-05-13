af2a5f62ab0751a5b50f29d9b347156b3071a9e4
Sachin7717 <sachindhimann71@gmail.com>
2024-05-13 22:18:09 +0530

    Im making my portfolio and this is jsx starting with left portion

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import "./App.css"
+import logo from "./bat.jpg"
+
+
+function App() {
+
+  return (
+    <>
+      <div className="body">body
+        <div className="left">
+          {/* <div className="me">
+            <img src={logo} alt=""  id="logo"/>
+            </div> */}
+            
+            <div className="info">
+              <h1>Sachin</h1>
+              <div className="contacts">
+              <h2>Contacts</h2>
+              <h3>Linkedin</h3>
+              <h3>twitter</h3>
+              <h3>github</h3>
+              <h3>insta</h3>
+              </div>
+
+            </div>
+
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
